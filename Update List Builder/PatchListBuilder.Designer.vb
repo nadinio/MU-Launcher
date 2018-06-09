dbc8e01269ed8a3f2efc60a7691b4dee6b02fe5c
N. Dinio <nika1667@gmail.com>
2018-06-09 13:50:47 -0400

    File list generator built  ML-8

---
+++ Update List Builder/PatchListBuilder.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class PatchListBuilder
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPath = New System.Windows.Forms.Label()
+        Me.txtDirectory = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.FolderExplorer = New System.Windows.Forms.FolderBrowserDialog()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblPath
+        '
+        Me.lblPath.AutoSize = True
+        Me.lblPath.Location = New System.Drawing.Point(12, 34)
+        Me.lblPath.Name = "lblPath"
+        Me.lblPath.Size = New System.Drawing.Size(55, 13)
+        Me.lblPath.TabIndex = 0
+        Me.lblPath.Text = "Directory: "
+        '
+        'txtDirectory
+        '
+        Me.txtDirectory.Location = New System.Drawing.Point(74, 34)
+        Me.txtDirectory.Name = "txtDirectory"
+        Me.txtDirectory.ReadOnly = True
+        Me.txtDirectory.Size = New System.Drawing.Size(182, 20)
+        Me.txtDirectory.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(111, 73)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 2
+        Me.btnBrowse.Text = "Browse..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(111, 164)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(75, 23)
+        Me.btnGenerate.TabIndex = 3
+        Me.btnGenerate.Text = "Generate!"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(125, 205)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(0, 13)
+        Me.lblResult.TabIndex = 4
+        '
+        'PatchListBuilder
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(307, 290)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.btnBrowse)
+        Me.Controls.Add(Me.txtDirectory)
+        Me.Controls.Add(Me.lblPath)
+        Me.Name = "PatchListBuilder"
+        Me.Text = "Patch List Builder"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblPath As Label
+    Friend WithEvents txtDirectory As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents btnGenerate As Button
+    Friend WithEvents FolderExplorer As FolderBrowserDialog
+    Friend WithEvents lblResult As Label
+End Class
---
+++ Update List Builder/PatchListBuilder.vb
@@ -0,0 +1,32 @@
+﻿Imports System.IO
+
+Public Class PatchListBuilder
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If (FolderExplorer.ShowDialog() = DialogResult.OK) Then
+            txtDirectory.Text = FolderExplorer.SelectedPath
+        End If
+    End Sub
+
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim files As String() = Directory.GetFiles(txtDirectory.Text, "*", SearchOption.AllDirectories)
+
+        If System.IO.File.Exists(txtDirectory.Text & "filelist.txt") = True Then
+            System.IO.File.Delete(txtDirectory.Text & "filelist.txt")
+            System.IO.File.Create(txtDirectory.Text & "filelist.txt").Close()
+        Else
+            System.IO.File.Create(txtDirectory.Text & "filelist.txt").Close()
+        End If
+
+        Dim textWriter As New System.IO.StreamWriter(txtDirectory.Text & "filelist.txt")
+        textWriter.WriteLine("[File List]")
+
+        For i As Integer = 0 To files.Length - 1
+            files(i) = files(i).Replace(txtDirectory.Text, "")
+            textWriter.WriteLine(files(i))
+        Next
+
+        textWriter.Close()
+        lblResult.Text = "File list generated successfully!"
+
+    End Sub
+End Class
